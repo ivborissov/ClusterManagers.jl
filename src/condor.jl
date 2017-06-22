21da4ea14c56c5997ecd63ceddbc7bc54461ed86
ivborissov <iv.borissov@gmail.com>
2017-06-22 17:37:09 +0300

    Add files via upload
    This draft version of condor.jl is the version that supports windows environment (both master and execute nodes)
    
    Pre-requisites:
    1. Julia is installed in the dir accessible by HTCondor
    2. system variable JULIA_PKGDIR is set to the path where Julia is installed (ex. C:\Julia)
    3. Ncat is installed on remote machines (nmap.org) to C:\\Program Files (x86)\\Nmap
    4. '.julia-htc' dir is created in julia home (==JULIA_PKGDIR)

--- src/condor.jl
+++ src/condor.jl
@@ -10,33 +10,39 @@ function condor_script(portnum::Integer, np::Integer, params::Dict)
     dir = params[:dir]
     exename = params[:exename]
     exeflags = params[:exeflags]
-    home = ENV["HOME"]
-    hostname = ENV["HOSTNAME"]
+	home = ENV["JULIA_PKGDIR"]
+	hostname = "$(gethostname())"
     jobname = "julia-$(getpid())"
-    tdir = "$home/.julia-htc"
-    run(`mkdir -p $tdir`)
+    tdir = "$home\\.julia-htc"
+#   run(`cmd.exe /c mkdir $tdir`)
 
-    scriptf = open("$tdir/$jobname.sh", "w")
-    println(scriptf, "#!/bin/sh")
-    println(scriptf, "cd $(Base.shell_escape(dir))")
-    println(scriptf, "$(Base.shell_escape(exename)) $(Base.shell_escape(worker_arg)) | /usr/bin/telnet $(Base.shell_escape(hostname)) $portnum")
+    scriptf = open("$tdir\\$jobname.bat", "w")
+	println(scriptf, "set HOMEDRIVE=$home")
+	println(scriptf, "set HOMEPATH=$home")
+#	println(scriptf, "set JULIA_NUM_THREADS=%NUMBER_OF_PROCESSORS%")
+	println(scriptf, "set Path=C:\\Program Files (x86)\\Nmap")
+    println(scriptf, "cd $home\\bin")
+#   println(scriptf, "julia.exe $(Base.shell_escape(worker_arg)) | telnet $(Base.shell_escape(hostname)) $portnum")
+    println(scriptf, "julia.exe $(Base.shell_escape(worker_arg)) | ncat $(Base.shell_escape(hostname)) $portnum")
     close(scriptf)
 
-    subf = open("$tdir/$jobname.sub", "w")
-    println(subf, "executable = /bin/bash")
-    println(subf, "arguments = ./$jobname.sh")
+    subf = open("$tdir\\$jobname.sub", "w")
+	println(subf, "executable = $tdir\\$jobname.bat")
+	println(subf, "requirements = Machine != \"$hostname\"")
     println(subf, "universe = vanilla")
     println(subf, "should_transfer_files = yes")
-    println(subf, "transfer_input_files = $tdir/$jobname.sh")
+ 	println(subf, "when_to_transfer_output = ON_EXIT")
+    println(subf, "transfer_input_files = $tdir\\$jobname.bat")
     println(subf, "Notification = Error")
     for i = 1:np
-        println(subf, "output = $tdir/$jobname-$i.o")
-        println(subf, "error= $tdir/$jobname-$i.e")
+        println(subf, "output = $tdir\\$jobname-$i.o")
+        println(subf, "error = $tdir\\$jobname-$i.e")
+		println(subf, "log = $tdir\\$jobname-$i.l")
         println(subf, "queue")
     end
     close(subf)
 
-    "$tdir/$jobname.sub"
+    "$tdir\\$jobname.sub"
 end
 
 function launch(manager::HTCManager, params::Dict, instances_arr::Array, c::Condition)
