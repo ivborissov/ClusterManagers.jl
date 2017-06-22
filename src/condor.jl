42ea735048b2b1fc5cc3d7c5dd43228f8ed08eb3
ivborissov <iv.borissov@gmail.com>
2017-06-22 17:39:29 +0300

    Update condor.jl

--- src/condor.jl
+++ src/condor.jl
@@ -10,34 +10,34 @@ function condor_script(portnum::Integer, np::Integer, params::Dict)
     dir = params[:dir]
     exename = params[:exename]
     exeflags = params[:exeflags]
-	home = ENV["JULIA_PKGDIR"]
-	hostname = "$(gethostname())"
+    home = ENV["JULIA_PKGDIR"]
+    hostname = "$(gethostname())"
     jobname = "julia-$(getpid())"
     tdir = "$home\\.julia-htc"
 #   run(`cmd.exe /c mkdir $tdir`)
 
     scriptf = open("$tdir\\$jobname.bat", "w")
-	println(scriptf, "set HOMEDRIVE=$home")
-	println(scriptf, "set HOMEPATH=$home")
-#	println(scriptf, "set JULIA_NUM_THREADS=%NUMBER_OF_PROCESSORS%")
-	println(scriptf, "set Path=C:\\Program Files (x86)\\Nmap")
+    println(scriptf, "set HOMEDRIVE=$home")
+    println(scriptf, "set HOMEPATH=$home")
+#   println(scriptf, "set JULIA_NUM_THREADS=%NUMBER_OF_PROCESSORS%")
+    println(scriptf, "set Path=C:\\Program Files (x86)\\Nmap")
     println(scriptf, "cd $home\\bin")
 #   println(scriptf, "julia.exe $(Base.shell_escape(worker_arg)) | telnet $(Base.shell_escape(hostname)) $portnum")
     println(scriptf, "julia.exe $(Base.shell_escape(worker_arg)) | ncat $(Base.shell_escape(hostname)) $portnum")
     close(scriptf)
 
     subf = open("$tdir\\$jobname.sub", "w")
-	println(subf, "executable = $tdir\\$jobname.bat")
-	println(subf, "requirements = Machine != \"$hostname\"")
+    println(subf, "executable = $tdir\\$jobname.bat")
+    println(subf, "requirements = Machine != \"$hostname\"")
     println(subf, "universe = vanilla")
     println(subf, "should_transfer_files = yes")
- 	println(subf, "when_to_transfer_output = ON_EXIT")
+    println(subf, "when_to_transfer_output = ON_EXIT")
     println(subf, "transfer_input_files = $tdir\\$jobname.bat")
     println(subf, "Notification = Error")
     for i = 1:np
         println(subf, "output = $tdir\\$jobname-$i.o")
         println(subf, "error = $tdir\\$jobname-$i.e")
-		println(subf, "log = $tdir\\$jobname-$i.l")
+	println(subf, "log = $tdir\\$jobname-$i.l")
         println(subf, "queue")
     end
     close(subf)
